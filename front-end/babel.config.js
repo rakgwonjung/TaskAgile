89de3242658e2156f994c6488d40087adcc76a99
rakgwonjung <jrg8989@gmail.com>
2020-05-02 09:31:51 +0900

    vue unit test add

--- front-end/babel.config.js
+++ front-end/babel.config.js
@@ -4,3 +4,8 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+// module.exports = {
+//   presets: [
+//     '@vue/app'
+//   ]
+// }
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  lintOnSave: false,
   devServer: {
     port: 3000,
     // API 요청의 URL에 특정 패턴을 적용, 모든 API 요청은 /api/ 경로로 시작하게 만든다.
@@ -8,6 +9,16 @@ module.exports = {
       '/api/*': {
         target: 'http://localhost:8080'
       }
+    }
+  },
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      // 웹팩에 새로운 진입점을 만들어서 모든 서드파티 스타일을 하나의 .css 파일로 묶는다.
+      // 필요할 때마다 서드 파티 라이브러리의 스타일을 이곳에 추가할 것이다.
+      style: [
+        'bootstrap/dist/css/bootstrap.min.css'
+      ]
     }
   }
 }
